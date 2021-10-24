740ec49f498ef788c70457593a7ca4bdbe067cc8
SHIN-coder <68458732+SHIN25-coder@users.noreply.github.com>
2021-10-24 16:41:33 +0900

    Add files via upload

---
+++ Implementation/src/implementation/문자열.java
@@ -0,0 +1,30 @@
+package backJoon;
+
+import java.util.*;
+
+public class ¹®ÀÚ¿­ {
+
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.next();
+        String b = scanner.next();
+
+        int result = a.length();
+
+        for(int i=0;i<b.length()-a.length()+1;i++){
+            int tmp=0;
+
+            for(int j=0;j<a.length();j++){
+                if(a.charAt(j)!=b.charAt(j+i)){
+                    tmp++;
+                }
+            }
+            if(result > tmp){
+                result = tmp;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
